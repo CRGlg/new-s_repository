71c51fe26fee394f1a306365c60c23cbb2a821e3
CRGlg <142841680+CRGlg@users.noreply.github.com>
2025-05-16 21:23:13 -0300

    Update and rename isdex.php to index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+const NOME_CURSO = "Sistemas_de_Iformação";
+const INSTITUICAO = "Multivix";
+
+$semestre_atual = "7";
+
+
+$mensagen = "Estou no " . $semestre_atual . "° periodo do curso de " . NOME_CURSO . " a " . INSTITUICAO ;
+echo "<p> $mensagen </p>";
+
+
+
+
+$frutas = ["Banana", "Uva", "Melão"];
+
+
+?>
