b8a423423ddb0ecae8b7ad1bbbbfc14bcb0145bc
CRGlg <142841680+CRGlg@users.noreply.github.com>
2025-05-16 21:34:07 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,12 +7,16 @@ $semestre_atual = "7";
 
 
 $mensagen = "Estou no " . $semestre_atual . "° periodo do curso de " . NOME_CURSO . " a " . INSTITUICAO ;
-echo "<p> $mensagen </p>"; <br>
+echo "<p> $mensagen </p>";
 
 
 
 
 $frutas = ["Banana", "Uva", "Melão"];
+
+echo "A segunda fruta é: " . $frutas[1] . "<br>";
+
+
 
 
 ?>
