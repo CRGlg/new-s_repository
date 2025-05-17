02816eec22d90ac4e9fb4ffe7ca7736745326871
CRGlg <142841680+CRGlg@users.noreply.github.com>
2025-05-16 21:39:55 -0300

    Update index.php

--- index.php
+++ index.php
@@ -17,6 +17,16 @@ $frutas = ["Banana", "Uva", "Melão"];
 echo "A segunda fruta é: " . $frutas[1] . "<br>";
 
 
+$produto = array(
+    "nome" => "Elicoptero",
+    "preco" => 25000.00,
+    "estoque" => 12
+);
+
+echo "Produto: " . $produto["nome"] . "<br>";
+echo "Preço: R$ " . number_format($produto["preco"], 2, ',', '.') . "<br>";
+echo "Estoque: " . $produto["estoque"] . " unidades<br>";
+
 
 
 ?>
