5e93023bddedf2b285f3db3d9df627083e3d4db3
CRGlg <142841680+CRGlg@users.noreply.github.com>
2025-05-20 15:42:10 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,15 +2,19 @@
 
 const NOME_CURSO = "Sistemas_de_Iformação";
 const INSTITUICAO = "Multivix";
+const ANO_CORRENTE = "2025";
+const X0 = " ANO_CORRENTE - $ano_inicio_curso ";
 
 $semestre_atual = "7";
+$ano_inicio_curso ="2021";
 
 
 $mensagen = "Estou no " . $semestre_atual . "° periodo do curso de " . NOME_CURSO . " a " . INSTITUICAO ;
 echo "<p> $mensagen </p>";
 
-
-
+$mensagen1 = "Você está no curso há " . X0 . " anos.";
+echo "<p> $mensagen </p>;
+echo "<br>";
 
 $frutas = ["Banana", "Uva", "Melão"];
 
