e596fb0db46f5c23c1834ba383d8317536a3fc25
CRGlg <142841680+CRGlg@users.noreply.github.com>
2025-05-20 18:29:10 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <?php
 
 const NOME_CURSO = "Sistemas_de_Iformação";
@@ -13,10 +14,17 @@ $mensagen = "Estou no " . $semestre_atual . "° periodo do curso de " . NOME_CUR
 echo "<p> $mensagen </p>";
 
 $mensagen1 = "Você está no curso há " . X0 . " anos.";
-echo "<p> $mensagen </p>;
+echo "<p> $mensagen </p> . <br>";
+
+$mensagem_boas_vindas = 'Bem-vindo ao NOME_CURSO, semestre $semestre_atual.';
+echo "<h3>Tentativa com Aspas Simples:</h3>";
+echo "<p>" . $mensagem_boas_vindas . "</p>";
+
+$mensagem_corrigida_duplas = "Bem-vindo ao " . NOME_CURSO . ", semestre $semestre_atual.";
+echo "<p>" . $mensagem_corrigida_duplas . "</p>";
 
 
-$frutas = ["Banana", "Uva", "Melão"];
+$frutas = ['Banana', 'Uva', 'Melão'];
 
 echo "A segunda fruta é: " . $frutas[1] . "<br>";
 
@@ -27,10 +35,38 @@ $produto = array(
     "estoque" => 12
 );
 
+$produto["cor1"] = "verde";
+$produto["cor2"] = "azul";
+$produto["cor3"] = "vermelho";
+$produto["cor4"] = "preto";
+
 echo "Produto: " . $produto["nome"] . "<br>";
 echo "Preço: R$ " . number_format($produto["preco"], 2, ',', '.') . "<br>";
 echo "Estoque: " . $produto["estoque"] . " unidades<br>";
+echo "Cores disponiveis: " . $produto["cor1"] . ", " . $produto["cor2"] . ", " . $produto["cor3"] . ", " . $produto["cor4"] . "<br>";
 
+
+echo "Teremos desponivel no estoque: " . count(value: $produto) . " cores.<br>";
+echo "<pre>";
+var_dump(value: $produto);
+echo "</pre>";
+
+
+
+$carrinho =[ 
+    [
+    "Nome"=> "luminaria",
+    "preco"=> 65.00,
+    "quantidade"=> 2,
+],   
+    [
+        "Nome => "balde_tinta",
+
+
+        ]
+
+
+];
 
 
 ?>
