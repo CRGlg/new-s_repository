fbbc9e596dd56c5f81c11f313d1a40b9dd76c47a
CRGlg <142841680+CRGlg@users.noreply.github.com>
2025-05-23 19:50:45 -0300

    Create execicio3.php

---
+++ execicio3.php
@@ -0,0 +1,21 @@
+<?php 
+
+
+$idadeAluno = 20;
+
+if($idadeAluno >= 18){
+echo "Aluno maior de idade.<br>";
+} else { echo "Aluno menor de idade.<br>"
+;}
+
+
+$cores = ["vermelho", "verde", "azul", "amarelo", "roxo"];
+echo "Novas cores: <br>;
+foreach ($cores as $cor) {
+echo $cor . "<br>';
+
+}
+
+
+
+    ?>
